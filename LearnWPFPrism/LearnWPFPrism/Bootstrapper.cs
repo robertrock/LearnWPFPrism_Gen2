84721f13f8a1f9d037f4d6cf4ea40e83a8ecf884
Robert Thomas <RobertThomas31416@gmail.com>
2020-10-23 21:17:56 -0500

    initial commit

---
+++ LearnWPFPrism/LearnWPFPrism/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace LearnWPFPrism
+{
+    public  class Bootstrapper : UnityBootstrapper
+    {
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            base.Run(runWithDefaultConfiguration);
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            // set main window
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            Type sampleModuleType = typeof(SampleModule.SampleModule);
+
+            ModuleCatalog.AddModule(new Prism.Modularity.ModuleInfo
+            {
+                ModuleName = sampleModuleType.Name,
+                ModuleType = sampleModuleType.AssemblyQualifiedName
+            });
+        }
+
+    }
+}
---
+++ LearnWPFPrism/SampleModule/SampleModule.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+
+namespace SampleModule
+{
+    public class SampleModule : IModule
+    {
+        private IRegionManager _regionManager;
+    
+        public SampleModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            // binding a region to a view
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.SampleView)); 
+        }
+    }
+}
